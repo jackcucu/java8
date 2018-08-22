82e706996a9ff36e722acc00e28a9981afb8c29f
Cucu Eugeniu <eugeniu.cucu@atomate.net>
2018-08-22 10:51:19 +0300

    Move each task to self package

---
+++ src/main/java/md/jack/task11/StreamProcessor.java
@@ -0,0 +1,23 @@
+package md.jack.task11;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+// Reusable processFile method inspired by suggestion from
+// Jeff Lin of the Johns Hopkins Applied Physics Lab.
+
+@FunctionalInterface
+public interface StreamProcessor {
+  void processStream(Stream<String> strings);
+
+  static void processFile(String filename, StreamProcessor processor)
+  {
+    try(Stream<String> lines = Files.lines(Paths.get(filename))) {
+      processor.processStream(lines);
+    } catch(IOException ioe) {
+      System.err.println("Error reading file: " + ioe);
+    }
+  }
+}
